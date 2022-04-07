ec47730174f78811261c4908152bbfbe0befe8e7
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-04-07 16:24:21 +0900

    Add files via upload

---
+++ ProjectMountain--jun-/screens/FeedsScreen.js
@@ -0,0 +1,24 @@
+import React, {useContext} from 'react';
+import {StyleSheet, View, TextInput} from 'react-native';
+import FeedList from '../components/FeedList';
+import FloatingWriteButton from '../components/FloatingWriteButton';
+import LogContext from '../contexts/LogContext';
+
+function FeedScreen() {
+  const {logs} = useContext(LogContext);
+  console.log(JSON.stringify(logs, null, 2));
+  return (
+    <View style={styles.block}>
+      <FeedList logs={logs} />
+      <FloatingWriteButton />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+});
+
+export default FeedScreen;
---
+++ ProjectMountain--jun-/screens/MainTab.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import FeedScreen from './FeedsScreen';
+import CalendarScreen from './CalendarScreen';
+import SearchScreen from './SearchScreen';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {View, Text, StyleSheet} from 'react-native';
+import TransparentCircleButton from '../components/TransparentCircleButton';
+
+const Tab = createBottomTabNavigator();
+
+function MainTab() {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        showLabel: false,
+        activeTintColor: '#009688',
+      }}>
+      <Tab.Screen
+        name="Feeds"
+        component={FeedScreen}
+        options={{
+          headerLeft: () => (
+            <View style={styles.buttons}>
+              <TransparentCircleButton name="warning" color="red" />
+            </View>
+          ),
+          title: '로고',
+          headerTitleAlign: 'center',
+          headerRight: () => (
+            <View style={styles.buttons}>
+              <TransparentCircleButton name="person" color="#009688" />
+            </View>
+          ),
+
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Calendar"
+        component={CalendarScreen}
+        options={{
+          title: '카메라',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="photo-camera" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: '커뮤니티',
+          tabBarIcon: ({color, size}) => (
+            <Icon name="people" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  block: {
+    height: 48,
+    paddingHorizontal: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  buttons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
+
+export default MainTab;
