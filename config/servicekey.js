d0fbc9078b0a1bdab9d822278c09d84559712afb
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-04-14 15:05:45 +0900

    Delete config directory

--- config/servicekey.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-  ServiceKey: decodeURIComponent(
-    'ApRVNc07DTbnR3THN3mKXiQ7bUdMoEWopO1aAqQYS%2BdL7i%2FslCo0aT2gu29DHZ%2FIGPMR9%2FZqQ5ty2KmNRPe1zA%3D%3D'
-  ),
-};
--- config/url.js
+++
@@ -1,4 +0,0 @@
-module.exports={
-  mountainURI:
-  'http://apis.data.go.kr/1400000/service/cultureInfoService/mntInfoOpenAPI',
-};
