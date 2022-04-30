ea61f4ad6d3886414be3b186e96e930f59539c3d
leejae7124 <leejae7124@gmail.com>
2022-04-30 22:51:52 +0900

    test

---
+++ model/li.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var listschema = new Schema({
+    name:{
+        type: Array
+    },
+    list:{
+        type: Array
+    }
+});
+
+module.exports = {mountain_list : mongoose.model('mountian_list', listschema)}
