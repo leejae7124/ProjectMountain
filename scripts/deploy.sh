31d5afb95d4a36db2a6874e74cad1baa769d083d
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-05-02 21:02:17 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ec2-user/test
-#pm2 kill
+sudo pm2 kill
 cd $REPOSITORY
 
 sudo npm install
-#pm2 kill
-#pm2 start test.js
+sudo pm2 kill
+sudo pm2 start test.js
 #cd ${REPO}
 
-sudo npm install
+#sudo npm install
 
-npm run-script dev
+#npm run-script dev
