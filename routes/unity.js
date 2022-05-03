8d7b7419fc995c156c5946d5cd4a617f3cd5ed35
SeungahhHam <saham7532@kyonggi.ac.kr>
2022-05-03 19:18:38 +0900

    yes

--- routes/unity.js
+++ routes/unity.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/get', (req, res) => {
     res.send('hi unity')
+})
+
+router.post('/post', (req, res) => {
+    console.log(req.body.message)
+    if(req.body.message == 'android')
+        res.send('success')
 })
 
 module.exports = router
