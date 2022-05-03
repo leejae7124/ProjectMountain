e9ea6452b3a81010a0d0e942002d91404a0c3c54
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-05-03 15:54:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ app.use('/api/user', require('./routes/usermanage'))
 app.use('/api/community/free', require('./routes/bordF')) 
 app.use('/api/community/certify', require('./routes/bordC')) 
 app.use('/api/community/question', require('./routes/bordQ')) 
-app.use('/api/community/recruit', require('./routes/bordR'))
 
 //route handling for mountinfo
 app.use('/api/mountInfo', require('./routes/mountInfo'))
@@ -37,56 +36,4 @@ app.get('/', (req, res) => {
 
 //Port
 const port = 5000
-const server = app.listen(port, () => console.log('Server is running'));
-
-
-//socket server
-const path = require('path');
-const morgan = require('morgan');
-//const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
-
-dotenv.config();
-//const webSocket = require('./routes/socket');
-//const indexRouter = require('./routes');
-
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-  express: app,
-  watch: true,
-});
-
-/*const sessionMiddleware = session({ //이런식으로 변수에 담아서 맨 밑에 webSocket의 인자로 넘겨준다.
-  resave: false,
-  saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
-  cookie: {
-    httpOnly: true,
-    secure: false,
-  },
-});*/
-
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser(process.env.COOKIE_SECRET));
-//app.use(sessionMiddleware);
-
-//app.use('/', indexRouter);
-
-app.use((req, res, next) => {
-  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-  error.status = 404;
-  next(error);
-});
-
-app.use((err, req, res, next) => {
-  res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-//webSocket(server); //socket.js에 server를 넣었으므로 express 서버와 websocket 서버랑 연결이 된다.
+app.listen(port, () => console.log('Server is running'));
