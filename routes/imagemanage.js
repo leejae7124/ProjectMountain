481317e8b8abefb4ad757c354bec18a01504db1d
SeungahhHam <102564850+SeungahhHam@users.noreply.github.com>
2022-05-05 20:24:25 +0900

    Update imagemanage.js

--- routes/imagemanage.js
+++ routes/imagemanage.js
@@ -3,12 +3,8 @@ const router = express.Router();
 const multer = require('multer');
 const multerS3 = require('multer-s3')
 const AWS = require("aws-sdk");
+const s3 = require('../config/s3');
 
-const s3 = new AWS.S3({ 
-    accessKeyId: 'AKIATXM7D6M33S6VWN7C', 
-    secretAccessKey: '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', 
-    region: 'ap-northeast-2', 
-});
 
 const storage = multerS3({ 
     s3: s3,
