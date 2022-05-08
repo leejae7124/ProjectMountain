ac7614787102afd51c82ffefd4d95fe049ff65c3
SeungahhHam <saham7532@kyonggi.ac.kr>
2022-05-08 21:16:50 +0900

    y

--- routes/map.js
+++ routes/map.js
@@ -4,9 +4,10 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.render('map', {
         javascriptkey: '770f232b413de4c27700024cd1dfc080',
-        x: 37.44447311198552,
-        y: 126.96388893520306
+        x: req.body.x,
+        y: req.body.y
     })
+    res.json({message: 'yes'})
 })
 
 module.exports = router
