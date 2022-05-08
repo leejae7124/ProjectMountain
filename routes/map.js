d0d067b40975bc2e5f9e98276daa420f9299b53e
SeungahhHam <saham7532@kyonggi.ac.kr>
2022-05-08 22:10:49 +0900

    y

--- routes/map.js
+++ routes/map.js
@@ -1,14 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+// Define Schemes
+const schema = new mongoose.Schema({
+  x: { type: Number},
+  y: { type: Number },
+});
+const map = mongoose.model('loc', schema)
+
+router.get('/add', (req, res) => {
+    map.collection.insertOne(req.body)
+    res.send('fin')
+})
 
 router.get('/', (req, res, next) => {
-    console.log(req.body)
-    res.render('map', {
-        javascriptkey: '770f232b413de4c27700024cd1dfc080',
-        x: Number(req.body.x),
-        y: Number(req.body.y)
+    map.collection.updateOne({_id: "map"},{$set: {x: req.body.x, y: req.body.y}}, function(error, docs){
+        if(error){
+            console.log(error);
+        }else{
+          console.log('yes')
+        }
     })
-    res.json({message: 'yes'})
+    map.collection.findOne({_id: "map"}, function(error, docs){
+        if(error){
+            console.log(error);
+        }else{
+            res.render('map', {
+                javascriptkey: '770f232b413de4c27700024cd1dfc080',
+                x: parseFloat(docs.x),
+                y: parseFloat(docs.y)
+            })
+        }
+    })
 })
 
 module.exports = router
