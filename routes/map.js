228574f42ec73ff234b0d960d0d06668a0bf6c28
SeungahhHam <saham7532@kyonggi.ac.kr>
2022-05-08 22:25:03 +0900

    y

--- routes/map.js
+++ routes/map.js
@@ -14,7 +14,7 @@ router.get('/add', (req, res) => {
     res.send('fin')
 })
 
-router.get('/update', (req, res, next) => {
+router.post('/update', (req, res, next) => {
     map.collection.updateOne({_id: "map"},{$set: {x: req.body.x, y: req.body.y}}, function(error, docs){
         if(error){
             res.send(error);
