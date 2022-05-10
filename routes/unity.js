d456057fd2c249073654573b532d80e74ebce9ce
SeungahhHam <saham7532@kyonggi.ac.kr>
2022-05-10 11:22:54 +0900

    y

--- routes/unity.js
+++ routes/unity.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { auth } = require('../middleware/auth');
+const mongoose = require('mongoose');
 const Mount = require('../model/Mountain.js');
 const Mount_loc_schema = 
 [   Mount.mountainSeoul, 
@@ -20,12 +20,55 @@ const Mount_loc_schema =
     Mount.mountainGyeongnam,
     Mount.mountainJeju
 ]
+const { auth } = require('../middleware/auth');
+const { User } = require("../model/User");
+
+// Define Schemes
+const schema = new mongoose.Schema({
+  x: { type: Number},
+  y: { type: Number },
+});
+const unity = mongoose.model('unity', schema)
+
+router.get('/add', (req, res) => {
+    unity.collection.insertOne(req.body)
+    res.send('fin')
+})
 
 router.post('/client', auth, (req, res) => {
-
+    //리스트에서 뽑은 id가 지역별 collection에 있는지 확인
+    for(let j = 0; j < 16; j++){
+        Mount_loc_schema[j].findOne({"$and" :[{x: {"$gte": parseFloat(req.body.x)-0.001, "$lte": parseFloat(req.body.x)+0.001}},
+                                    {y: {"$gte": parseFloat(req.body.y)-0.001, "$lte": parseFloat(req.body.y)+0.001}}]}
+           , (err, docs) => {
+            if (err) console.log(err) 
+            if(docs != null) {
+                //산을 찾았을 경우 보낼 메세지 업데이트
+                unity.collection.updateOne({ _id: "unity"}, {$set: {mntnnm: docs.mntnnm}})
+                //산을 찾았을 경우 user 업데이트
+                User.collection.updateOne({email: req.user.email}, {$push: {badge: String(docs.mntnid)}})
+                User.collection.findOne({email: req.user.email}, function(error, result){
+                if(error){
+                    console.log(error);
+                }else{
+                    if(result.badge.length <= 10) {
+                        User.collection.updateOne({email: req.user.email}, {$set: {level: 1, badgeProgress: result.badge.length/10}})
+                    }
+                    else if(result.badge.length <= 100) {
+                        User.collection.updateOne({email: req.user.email}, {$set: {level: 2, badgeProgress: result.badge.length/100}})
+                    }
+                    else if(result.badge.length <= 1000) {
+                        User.collection.updateOne({email: req.user.email}, {$set: {level: 3, badgeProgress: result.badge.length/1000}})
+                    }   
+                }
+            })      
+        }
+    })
+    }
     res.send({message: 'fin'})
 })
 
+//unity에 산 이름 보내주기
 router.get('/get', (req, res) => {
     unity.collection.findOne({_id: "unity"} , function(error, docs){
         if(error){
@@ -36,10 +79,12 @@ router.get('/get', (req, res) => {
       })
 })
 
+//unity에서 성공 메시지 받기
 router.post('/post', (req, res) => {
     console.log(req.body.message)
-    if(req.body.message == 'android')
+    if(req.body.message == 'android') //메시지 변경 필요
         res.send('success')
+    else res.send('fail')
 })
 
 module.exports = router
